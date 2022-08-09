3fc7f4deb45961de7c839fa8373ac97446468f98
Bernardo C. G. da Silva <bernardocgdasilva@gmail.com>
2022-08-09 00:59:49 -0300

    Pane Bem-estar

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Dixit-Stiglitz_Analise_Grafica
-Shiny dashboard para análise gráfica do modelo de concorrência monopolística de Dixit-Stiglitz
+
+Shiny dashboard para análise gráfica do modelo de concorrência monopolística de Dixit-Stiglitz:
+
+<https://bernardocgdasilva.shinyapps.io/Modelo_de_Dixit_Stiglitz>
--- app.R
+++ app.R
@@ -5,30 +5,57 @@
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
+library(rsconnect)
+
+###################################################################################
+#                                     UI
+###################################################################################
 
 header <- dashboardHeader(title = "Modelo de Dixit-Stiglitz", titleWidth = 300)
 
 sidebar <- dashboardSidebar(
   width = 300,
-  sliderInput("alpha", "Custo fixo", 1, 100, 1),
-  sliderInput("beta", "Custo variável", 1, 100, 1),
-  sliderInput("L", "População", 1, 10000, 1),
-  sliderInput("epsilon", "Elasticidade de substituição", 1, 20, 1),
-  sliderInput("delta", "Parte da renda gasta em manufaturados", 0, 1, 0.5),
-  sliderInput("gamma", "Parte da população em manufaturados", 0, 1, 0.5)
+  sliderInput("alpha", "Custo fixo", min = 1, max = 1000, value = 100, step = 1, animate = animationOptions(interval = 3000)),
+  sliderInput("beta", "Custo variável", min = 1, max = 1000, value = 100, step = 1, animate = animationOptions(interval = 3000)),
+  sliderInput("L", "População", min = 1, max = 10000, value = 1000, step = 1, animate = animationOptions(interval = 3000)),
+  sliderInput("epsilon", "Elasticidade de substituição", min = 1, max = 20, value = 2, step = 1, animate = animationOptions(interval = 3000)),
+  sliderInput("delta", "Parte da renda gasta em manufaturados", min = 0, max = 1, value = 0.5, step = 0.05, animate = animationOptions(interval = 3000)),
+  sliderInput("gamma", "Parte da população em manufaturados", min = 0, max = 1, value = 0.5, step = 0.05, animate = animationOptions(interval = 3000))
 )
 
 body <- dashboardBody(
   fluidRow(
     box(
-      width = 6,
+      width = 4,
       plotOutput("bem_estar_gamma"),
       title = "Bem-estar x Gamma"
     ),
     box(
-      width = 6,
+      width = 4,
       plotOutput("bem_estar_delta"),
       title = "Bem-estar x Delta"
+    ),
+    box(
+      width = 4,
+      plotOutput("bem_estar_epsilon"),
+      title = "Bem-estar x Epsilon"
+    )
+  ),
+  fluidRow(
+    box(
+      width = 4,
+      plotOutput("bem_estar_alpha"),
+      title = "Bem-estar x Alpha"
+    ),
+    box(
+      width = 4,
+      plotOutput("bem_estar_beta"),
+      title = "Bem-estar x Beta"
+    ),
+    box(
+      width = 4,
+      plotOutput("bem_estar_L"),
+      title = "Bem-estar x L"
     )
   )
 )
@@ -62,8 +89,10 @@ server <- function(input, output) {
             epsilon = d,
             delta = e
           )
-        )+
-        theme_classic()
+        ) +
+        theme_classic() +
+        labs(x = expression(gamma), y = "Bem-estar")+
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14))
     }
     gg(input$alpha, input$beta, input$L, input$epsilon, input$delta)
   })
@@ -82,20 +111,102 @@ server <- function(input, output) {
             gamma = e
           )
         ) +
-        theme_classic()
+        theme_classic() +
+        labs(x = expression(delta), y = "Bem-estar")+
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14))
     }
     gg(input$alpha, input$beta, input$L, input$epsilon, input$gamma)
   })
 
-  }
+  # epsilon
+  output$bem_estar_epsilon <- renderPlot({
+    gg <- function(a, b, c, d, e) {
+      ggplot(data.frame(epsilon = c(1, 20)), aes(epsilon)) +
+        stat_function(
+          fun = bem_estar,
+          args = list(
+            alpha = a,
+            beta = b,
+            L = c,
+            delta = d,
+            gamma = e
+          )
+        ) +
+        theme_classic() +
+        labs(x = expression(epsilon), y = "Bem-estar")+
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14))
+    }
+    gg(input$alpha, input$beta, input$L, input$delta, input$gamma)
+  })
+
+  # alpha
+  output$bem_estar_alpha <- renderPlot({
+    gg <- function(a, b, c, d, e) {
+      ggplot(data.frame(alpha = c(1, 1000)), aes(alpha)) +
+        stat_function(
+          fun = bem_estar,
+          args = list(
+            epsilon = a,
+            beta = b,
+            L = c,
+            delta = d,
+            gamma = e
+          )
+        ) +
+        theme_classic() +
+        labs(x = expression(alpha), y = "Bem-estar")+
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14))
+    }
+    gg(input$epsilon, input$beta, input$L, input$delta, input$gamma)
+  })
+
+  # beta
+  output$bem_estar_beta <- renderPlot({
+    gg <- function(a, b, c, d, e) {
+      ggplot(data.frame(beta = c(1, 1000)), aes(beta)) +
+        stat_function(
+          fun = bem_estar,
+          args = list(
+            epsilon = a,
+            alpha = b,
+            L = c,
+            delta = d,
+            gamma = e
+          )
+        ) +
+        theme_classic() +
+        labs(x = expression(beta), y = "Bem-estar") +
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14))
+    }
+    gg(input$epsilon, input$alpha, input$L, input$delta, input$gamma)
+  })
+
+  # L
+  output$bem_estar_L <- renderPlot({
+    gg <- function(a, b, c, d, e) {
+      ggplot(data.frame(L = c(1, 10000)), aes(L)) +
+        stat_function(
+          fun = bem_estar,
+          args = list(
+            epsilon = a,
+            alpha = b,
+            beta = c,
+            delta = d,
+            gamma = e
+          )
+        ) +
+        theme_classic() +
+        labs(x = "L", y = "Bem-estar")+
+        theme(axis.text = element_text(size = 14), axis.title = element_text(size = 14))
+    }
+    gg(input$epsilon, input$alpha, input$beta, input$delta, input$gamma)
+  })
+}
 
 shinyApp(ui, server)
 
+###################################################################################
+#                                     DEPLOY
+###################################################################################
 
-
-
-
-# fx <- function(x,p){
-#  x*p**2
-# }
-# ggplot()+stat_function(fun = fx, args = list(x=5))
+#rsconnect::deployApp(account = "bernardocgdasilva", appName = "Modelo_de_Dixit_Stiglitz")
